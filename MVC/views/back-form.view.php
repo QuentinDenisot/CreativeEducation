d3defd847d3177074a14c8228fb7f5f02dc0a4f4
théo <theosen95@gmail.com>
2018-05-10 16:44:05 +0200

    changement views

---
+++ MVC/views/back-form.view.php
@@ -0,0 +1,84 @@
+<div class="main-panel">
+    <!-- <nav class="navbar navbar-transparent navbar-absolute">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                    </div>
+                </div>
+            </nav> -->
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header card-header-icon" data-background-color="rose">
+                            <i class="material-icons">contacts</i>
+                        </div>
+                        <div class="card-content">
+                            <h4 class="card-title">Formulaire de modification</h4>
+                            <form class="form-horizontal">
+                                <div class="row">
+                                    <label class="col-md-2 label-on-left">Nom</label>
+                                    <div class="col-md-10">
+                                        <div class="form-group is-empty">
+                                            <label class="control-label"></label>
+                                            <input type="email" class="form-control" placeholder="Nom de la personne.">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <label class="col-md-2 label-on-left">Prénom</label>
+                                    <div class="col-md-10">
+                                        <div class="form-group is-empty">
+                                            <label class="control-label"></label>
+                                            <input type="email" class="form-control" placeholder="Prénom de la personne.">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <label class="col-md-2 label-on-left">Adresse e-mail</label>
+                                    <div class="col-md-10">
+                                        <div class="form-group is-empty">
+                                            <label class="control-label"></label>
+                                            <input type="email" class="form-control" placeholder="Adresse mail de la personne.">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <label class="col-md-2 label-on-left">Rôle</label>
+                                    <div class="col-md-10">
+                                        <div class="form-group is-empty">
+                                            <label class="control-label"></label>
+                                            <input type="email" class="form-control" placeholder="Rôle de la personne.">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <label class="col-md-2 label-on-left">Mot de passe</label>
+                                    <div class="col-md-10">
+                                        <div class="form-group is-empty">
+                                            <label class="control-label"></label>
+                                            <input type="password" class="form-control">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group form-button form-button-center">
+                                            <button type="submit" class="btn btn-fill btn-rose">Valider les modifications</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
