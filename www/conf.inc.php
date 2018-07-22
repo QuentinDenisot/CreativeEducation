dca4cdc27b59bca7cfbc6da303a9190609344596
théo <theosen95@gmail.com>
2018-07-22 03:58:16 +0200

    redo

--- www/conf.inc.php
+++ www/conf.inc.php
@@ -1,12 +1,11 @@
  <?php
     define("DBUSER", "root");
     define("DBHOST", "db");
-    define("DBNAME", "creativeedu");
+    define("DBNAME", "myDb");
     define("DBPWD", "test");
     define("DBPORT", "3306");
     define("DBDRIVER", "mysql");
-    define("SERVERNAME", $_SERVER['SERVER_NAME']);
-
+    
     define("DS", "/");
     $scriptName = (dirname($_SERVER["SCRIPT_NAME"]) == "/") ? "" : dirname($_SERVER["SCRIPT_NAME"]);
     define("DIRNAME", $scriptName.DS);
